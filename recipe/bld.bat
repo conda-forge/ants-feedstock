44894a5e9b73b252ebb06165434a079567b8039c
Ghislain Vaillant <ghisvail@users.noreply.github.com>
2023-06-22 21:20:52 +0200

    Update Windows manifest

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,30 @@
+:: Build out-of-tree.
 mkdir build
-if errorlevel 1 exit 1
 cd build
-if errorlevel 1 exit 1
 
+:: Config
 cmake -G Ninja ^
     %CMAKE_ARGS% ^
+    -DANTS_SUPERBUILD:BOOL=OFF ^
+    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DCMAKE_INSTALL_LIBDIR:STRING=%LIBRARY_LIB% ^
     -DCMAKE_INSTALL_PREFIX:STRING=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH:STRING=%LIBRARY_PREFIX% ^
-    -DBUILD_SHARED_LIBS:BOOL=ON ^
-    -DANTS_SUPERBUILD:BOOL=OFF ^
+    -DRUN_SHORT_TESTS:BOOL=ON ^
+    -DRUN_LONG_TESTS:BOOL=OFF ^
     -DUSE_SYSTEM_ITK:BOOL=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build . --config RelWithDebInfo --parallel %CPU_COUNT% --target install
+:: Build
+cmake --build . --config Release --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
+
+:: Test
+ctest -C Release --output-on-failure
+if errorlevel 1 exit 1
+
+:: Install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
